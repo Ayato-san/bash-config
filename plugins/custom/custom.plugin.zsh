adcd6a88a3516e5f5fc2469f88e3f21d2a33a84a
Ayato <ayatosan.pro@gmail.com>
2025-02-21 14:31:58 +0100

    Add custom Git utility functions for branch management

---
+++ plugins/custom/custom.plugin.zsh
@@ -0,0 +1,54 @@
+# Rename both local and remote git branch names
+# Usage: grename old_branch new_branch
+function grename() {
+  if [[ -z "$1" || -z "$2" ]]; then
+    echo "Usage: $0 old_branch new_branch"
+    return 1
+  fi
+
+  # Rename branch locally
+  git branch -m "$1" "$2"
+  # Rename branch in origin remote
+  if git push origin :"$1"; then
+    git push --set-upstream origin "$2"
+  fi
+}
+
+# Return current git branch name
+function current_branch() {
+  git_current_branch
+}
+
+# Return first branch name found from dev, devel, develop, development
+# If none found, return 'develop' with error code 1
+function develop_branch() {
+  command git rev-parse --git-dir &>/dev/null || return
+  local branch
+  for branch in dev devel develop development; do
+    if command git show-ref -q --verify refs/heads/$branch; then
+      echo $branch
+      return 0
+    fi
+  done
+
+  echo develop
+  return 1
+}
+
+# Return first branch name found from main, trunk, mainline, default, stable, master
+# Searches in local and remote (origin, upstream) references
+# If none found, return 'master' with error code 1
+function main_branch() {
+  command git rev-parse --git-dir &>/dev/null || return
+  local ref
+  for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk,mainline,default,stable,master}; do
+    if command git show-ref -q --verify $ref; then
+      echo ${ref:t}
+      return 0
+    fi
+  done
+
+  # If no main branch was found, fall back to master but return error
+  echo master
+  return 1
+}
